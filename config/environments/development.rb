4765bdd3c57265cb39411161d4a0d08c34007fe8
takenokonyokki <97602717+takenokonyokki@users.noreply.github.com>
2022-02-13 03:06:26 +0000

    [Add]deviseのviewまで

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "e298dbb9394a4ef48a2e874212102be9.vfs.cloud9.us-east-1.amazonaws.com"
+
+  config.active_job.queue_adapter = :inline
 end
