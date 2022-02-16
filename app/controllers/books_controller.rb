78df2a152d647228f5b7bbaaec433c474de2c853
takenokonyokki <97602717+takenokonyokki@users.noreply.github.com>
2022-02-16 14:11:36 +0000

    [Add]部分テンプレート実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book.id)
   end
 
   def index
@@ -18,7 +18,8 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    #@user = @book.user
+    @books = Book.new
+    @user = @book.user
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @book = Book.new
+    @user = current_user
   end
 
   def show
     @user = User.find(params[:id])
-    @book = @user.books
+    @book = Book.new
+    @books = @user.books
   end
 
   def edit
