7421bc2480fb905ad50992bedbea9fc3cb27655b
takenokonyokki <97602717+takenokonyokki@users.noreply.github.com>
2022-02-17 14:39:12 +0000

    [Add]バリデーションの設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,14 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -24,6 +25,15 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
---
+++ db/migrate/20220217141525_rename_name_column_to_users.rb
@@ -0,0 +1,4 @@
+class RenameNameColumnToUsers < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
