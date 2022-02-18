3efe0786f016acd70ff715fc1dd411e0c72a2a6b
takenokonyokki <97602717+takenokonyokki@users.noreply.github.com>
2022-02-18 14:47:25 +0000

    [Add]サクセスメッセージ追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,20 @@
 class BooksController < ApplicationController
 
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
-      redirect_to book_path(@book.id)
-    else
-      @books = Book.all
-      render :index
-    end
-  end
-
   def index
     @books = Book.all
     @book = Book.new
     @user = current_user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -30,7 +30,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
     else
       render :edit
     end
