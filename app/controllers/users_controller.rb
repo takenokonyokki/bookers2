22d022f5546626a26126d0061e96d53dbe4e5d9c
takenokonyokki <97602717+takenokonyokki@users.noreply.github.com>
2022-02-20 07:42:22 +0000

    [Finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
       unless @user == current_user
-        redirect_to user_path(@user.id)
+        redirect_to user_path(current_user.id)
       end
   end
 
